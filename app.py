0eaa260d95f24da411a30d62925139ed351c5934
unknown <romulonilsonhorn@gmail.com>
2025-04-30 11:19:28 -0300

    permite pesquisa por id ou titulo

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ import requests
 app = Flask(__name__)
 connection_db = psycopg.connect('dbname=nerdgeeks user=postgres password=3f@db host=164.90.152.205 port=80')
 
-
-
-
 @app.route("/consultarnome/<title>", methods=["GET"])
 def consultar_nome(title):
+    if title[0] + title[1] == "tt":
+        r = requests.get(f'http://www.omdbapi.com/?apikey=221fdad0&i={title}')
+    else:
+        r = requests.get(f'http://www.omdbapi.com/?apikey=221fdad0&t={title}')
     cursor  = connection_db.cursor()
-    r = requests.get(f'http://www.omdbapi.com/?apikey=221fdad0&t={title}')
     id = r.json()["imdbID"]
     titulo = r.json()["Title"]
     year = r.json()["Year"]
