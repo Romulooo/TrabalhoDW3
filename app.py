e043027059612605eb1813d84fa33d0f465f1356
SoJoaomesmo <4540746781@estudante.sed.sc.gov.br>
2025-04-30 11:09:50 -0300

    Implementação funcional do PostGresSQL

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask
 import psycopg
 import requests
 
@@ -7,12 +7,48 @@ connection_db = psycopg.connect('dbname=nerdgeeks user=postgres password=3f@db h
 
 
 
+
 @app.route("/consultarnome/<title>", methods=["GET"])
 def consultar_nome(title):
-    cursor = connection_db.cursor()
-    cursor.execute('insert into filmes (titulo, id) values (%s, %s)', (title, id))
+    cursor  = connection_db.cursor()
     r = requests.get(f'http://www.omdbapi.com/?apikey=221fdad0&t={title}')
-    title = r.json()["Title"]
-    print(title)
-    return(r.text)
+    id = r.json()["imdbID"]
+    titulo = r.json()["Title"]
+    year = r.json()["Year"]
+    rated = r.json()["Rated"]
+    released = r.json()["Released"]
+    runtime = r.json()["Runtime"]
+    genre = r.json()["Genre"]
+    director = r.json()["Director"]
+    writer = r.json()["Writer"]
+    actors = r.json()["Actors"]
+    plot = r.json()["Plot"]
+    language = r.json()["Language"]
+    country = r.json()["Country"]
+    awards = r.json()["Awards"]
+    poster = r.json()["Poster"]
+    ratings = ""
+    for i in r.json()["Ratings"]:
+        ratings += (i["Source"]+ " : "+ i["Value"] + ", ")
+    metascore = r.json()["Metascore"]
+    imdbrating = r.json()["imdbRating"]
+    imdbvotes = r.json()["imdbVotes"]
+    type = r.json()["Type"]
 
+    sql = """
+    INSERT INTO filmes_series (
+        id, titulo, year, rated, released, runtime, genre, director,
+        writer, actors, plot, language, country, awards, poster,
+        ratings, metascore, imdbrating, imdbvotes, type
+    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+
+    values = (
+        id, titulo, year, rated, released, runtime, genre, director,
+        writer, actors, plot, language, country, awards, poster,
+        ratings, metascore, imdbrating, imdbvotes, type
+    )
+
+    cursor.execute(sql, values)
+    connection_db.commit()
+    return r.text
