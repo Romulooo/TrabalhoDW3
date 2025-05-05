99d76cb2e3fee3ea6e1032bfcfd1ee859484290e
Rômulo <139631832+Romulooo@users.noreply.github.com>
2025-05-05 20:53:53 -0300

    Documentação do código

--- app.py
+++ app.py
@@ -2,23 +2,36 @@ from flask import Flask
 import psycopg
 import requests
 
+# Armazena a aplicação em uma variável para inicia-lá depois
 app = Flask(__name__)
+
+# Armazena a conexão em uma variável, com ela já sendo excecutada
 connection_db = psycopg.connect('dbname=nerdgeeks user=postgres password=3f@db host=164.90.152.205 port=80')
 
+# Definição da rota consultar nome por título ou id
 @app.route("/consultarnome/<title>", methods=["GET"])
 def consultar_nome(title):
+
+    # Define o cursor
     cursor  = connection_db.cursor()
-    
+
+    # Verifica se foi passado um título ou um id (todos os ids iniciam com "tt")
     if title[0] + title[1] == "tt":
+        # Se for, faz a requisição pelo id
         r = requests.get(f'http://www.omdbapi.com/?apikey=221fdad0&i={title}')
     else:
+        # Se não, faz pelo título
         r = requests.get(f'http://www.omdbapi.com/?apikey=221fdad0&t={title}')
-    
+
+    # Verifica se o filme já está na tabela
     id = r.json()["imdbID"]
     cursor.execute(f"select count(*) from filmes_series where id = '{id}'")
     rows = cursor.fetchall()
+
+    # Se retornou algo, mostrar o texto
     if rows != [(0,)]:
         return r.text
+    # Se não, inclui na tabela
     else:
         titulo = r.json()["Title"]
         year = r.json()["Year"]
